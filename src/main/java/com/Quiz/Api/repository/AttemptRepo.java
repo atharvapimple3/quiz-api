dc86fb8cbcd64c8db11844192eab44c2007edcd0
atharvapimple3 <atharvapimple3@gmail.com>
2025-09-11 12:01:31 +0530

    modified the leaderboard for a tie-breaker case.

--- src/main/java/com/Quiz/Api/repository/AttemptRepo.java
+++ src/main/java/com/Quiz/Api/repository/AttemptRepo.java
@@ -2,7 +2,6 @@ package com.Quiz.Api.repository;
 
 import com.Quiz.Api.entities.Attempt;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +9,7 @@ import java.util.List;
 @Repository
 public interface AttemptRepo extends JpaRepository<Attempt, Integer> {
 
-    List<Attempt> findTop10ByQuiz_IdOrderByScoreDesc(Integer quizId);
+    List<Attempt> findTop10ByQuiz_IdOrderByScoreDescTimeTakenAsc(Integer quizId);
 
     List<Attempt> findAllByUser_Id(Integer userId);
 
